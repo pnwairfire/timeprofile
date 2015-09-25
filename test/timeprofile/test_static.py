1f9d9f6cb9eb4b217b40de26f1e51fda0c58a3b5
Joel Dubowy <jdubowy@gmail.com>
2015-09-25 14:31:50 -0700

    More WIP on tests for static profiler

--- test/timeprofile/test_static.py
+++ test/timeprofile/test_static.py
@@ -9,7 +9,8 @@ from py.test import raises
 
 from timeprofile.static import (
     StaticTimeProfiler,
-    InvalidStartEndTimesError
+    InvalidStartEndTimesError,
+    InvalidHourlyFractionsError
 )
 
 def assert_approximately_equal(expected, actual):
@@ -87,24 +88,29 @@ class TestStaticTimeProfiler_DefaultHourlyFractions(object):
 
     def test_one_day(self):
         # First make sure daily fractions are computed correctly
-        s = datetime.datetime(2015, 1, 1, 0)
-        e = datetime.datetime(2015, 1, 2, 0)
-        r = StaticTimeProfiler(s, e)
-        assert set(r.PHASES) == set(r.hourly_fractions.keys())
-        for p in r.PHASES:
-            assert_approximately_equal(r.DEFAULT_DAILY_HOURLY_FRACTIONS[p], r.hourly_fractions[p])
+        st = datetime.datetime(2015, 1, 1, 0)
+        et = datetime.datetime(2015, 1, 2, 0)
+        stp = StaticTimeProfiler(st, et)
+        #assert set(stp.PHASES) == set(stp.hourly_fractions.keys())
+        expected_hourly_fractions = {
+            p: stp.DEFAULT_DAILY_HOURLY_FRACTIONS[p] for p in stp.PHASES
+        }
+        assert_approximately_equal(expected_hourly_fractions, stp.hourly_fractions)
 
         # Now, test computation of profiled emissions
         # TODO: ....
 
     def test_two_days(self):
         # First make sure daily fractions are computed correctly
-        s = datetime.datetime(2015, 1, 1, 0)
-        e = datetime.datetime(2015, 1, 2, 0)
-        r = StaticTimeProfiler(s, e)
-        assert set(r.PHASES) == set(r.hourly_fractions.keys())
-        for p in r.PHASES:
-            assert_approximately_equal(r.DEFAULT_DAILY_HOURLY_FRACTIONS[p], r.hourly_fractions[p])
+        st = datetime.datetime(2015, 1, 1, 0)
+        et = datetime.datetime(2015, 1, 3, 0)
+        stp = StaticTimeProfiler(st, et)
+        #assert set(stp.PHASES) == set(stp.hourly_fractions.keys())
+        expected_hourly_fractions = {
+            p: 2 * map( lambda e: e / 2.0, stp.DEFAULT_DAILY_HOURLY_FRACTIONS[p])
+                for p in stp.PHASES
+        }
+        assert_approximately_equal(expected_hourly_fractions, stp.hourly_fractions)
 
         # Now, test computation of profiled emissions
         # TODO: ....
@@ -112,6 +118,7 @@ class TestStaticTimeProfiler_DefaultHourlyFractions(object):
     def test_partial_days(self):
         # First make sure daily fractions are computed correctly
         # TODO: ...
+
         # Now, test computation of profiled emissions
         # TODO: ...
         pass
@@ -141,25 +148,23 @@ class TestStaticTimeProfiler_CustomDailyHourlyFractions(object):
     }
 
     def test_one_day(self):
-        s = datetime.datetime(2015, 1, 1, 0)
-        e = datetime.datetime(2015, 1, 2, 0)
-        r = StaticTimeProfiler(s, e)
-        assert set(r.PHASES) == set(r.hourly_fractions.keys())
-        for p in r.PHASES:
-            assert_approximately_equal(self.DAILY_HOURLY_FRACTIONS[p], r.hourly_fractions[p])
+        st = datetime.datetime(2015, 1, 1, 0)
+        et = datetime.datetime(2015, 1, 2, 0)
+        stp = StaticTimeProfiler(st, et, hourly_fractions=self.DAILY_HOURLY_FRACTIONS)
+        #assert set(stp.PHASES) == set(stp.hourly_fractions.keys())
+        expected_hourly_fractions = {
+            p: self.DAILY_HOURLY_FRACTIONS[p] for p in stp.PHASES
+        }
+        assert_approximately_equal(expected_hourly_fractions, stp.hourly_fractions)
 
-        # assert_approximately_equal(r.hourly_fractions,
-        #     StaticTimeProfiler.DEFAULT_DAILY_HOURLY_FRACTIONS)
-
-        # stp = StaticTimeProfiler(self.DAILY_HOURLY_FRACTIONS)
-        # r = stp._compute_hourly_fractions(s, e)
-        # assert_approximately_equal(r,
-        #     self.DAILY_HOURLY_FRACTIONS)
+        # Now, test computation of profiled emissions
+        # TODO: ....
 
     def test_two_days(self):
-        # s = datetime.datetime(2015, 1, 1, 0)
-        # e = datetime.datetime(2015, 1, 3, 0)
-        # r = StaticTimeProfiler()._compute_hourly_fractions(s, e)
+        st = datetime.datetime(2015, 1, 1, 0)
+        et = datetime.datetime(2015, 1, 3, 0)
+        stp = StaticTimeProfiler(st, et, hourly_fractions=self.DAILY_HOURLY_FRACTIONS)
+
         # expected = 2* map(lambda x: x / 2,
         #     StaticTimeProfiler.DEFAULT_DAILY_HOURLY_FRACTIONS)
         # assert_approximately_equal(r, expected)
@@ -213,3 +218,63 @@ class TestStaticTimeProfiler_CustomDailyHourlyFractions(object):
         # TODO: add assert
 
         pass
+
+class TestStaticTimeProfiler_CustomHourlyFractions(object):
+
+    # 48 hours worth of hourly fractions
+    HOURLY_FRACTIONS = {
+        'flaming': [
+            0.005, 0.005, 0.01, 0.01, 0.01, 0.01,
+            0.05, 0.05, 0.05, 0.05, 0.05, 0.05,
+            0.05, 0.10, 0.05, 0.05, 0.05, 0.05,
+            0.05, 0.05, 0.05, 0.05, 0.03, 0.02,
+            0.02, 0.02, 0.01, 0.00, 0.00, 0.00,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+        ],
+        'residual': [  # shifted 6 hrs
+            0.05, 0.05, 0.05, 0.05, 0.03, 0.02,
+            0.005, 0.005, 0.01, 0.01, 0.01, 0.01,
+            0.05, 0.05, 0.05, 0.05, 0.05, 0.05,
+            0.05, 0.05, 0.05, 0.05, 0.05, 0.05,
+            0.05, 0.05, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+        ],
+        'smoldering': [  # shifted another 6 hrss
+            0.10, 0.10, 0.05, 0.05, 0.05, 0.05,
+            0.05, 0.05, 0.05, 0.05, 0.03, 0.02,
+            0.005, 0.005, 0.01, 0.01, 0.01, 0.01,
+            0.05, 0.05, 0.05, 0.05, 0.05, 0.05,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+        ]
+    }
+
+    def test_one_day(self):
+        st = datetime.datetime(2015, 1, 1, 0)
+        et = datetime.datetime(2015, 1, 2, 0)
+        with raises(InvalidHourlyFractionsError) as e:
+            stp = StaticTimeProfiler(st, et, hourly_fractions=self.HOURLY_FRACTIONS)
+
+    def test_two_days(self):
+        st = datetime.datetime(2015, 1, 1, 0)
+        et = datetime.datetime(2015, 1, 3, 0)
+        stp = StaticTimeProfiler(st, et, hourly_fractions=self.HOURLY_FRACTIONS)
+        expected_hourly_fractions = {
+            p: self.HOURLY_FRACTIONS[p] for p in stp.PHASES
+        }
+        assert_approximately_equal(expected_hourly_fractions, stp.hourly_fractions)
+
+        # Now, test computation of profiled emissions
+        # TODO: ...
+
+    def test_three_days(self):
+        st = datetime.datetime(2015, 1, 1, 0)
+        et = datetime.datetime(2015, 1, 4, 0)
+        with raises(InvalidHourlyFractionsError) as e:
+            stp = StaticTimeProfiler(st, et, hourly_fractions=self.HOURLY_FRACTIONS)
