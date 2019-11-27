146184aa31ecd1ad466f0108240499d4fc98d417
Joel Dubowy <jdubowy@gmail.com>
2019-11-27 12:33:59 -0800

    Update FepsTimeProfiler to validate fire_type and add support for WF

--- timeprofile/feps.py
+++ timeprofile/feps.py
@@ -29,10 +29,13 @@ import math
 
 from . import BaseTimeProfiler, InvalidStartEndTimesError
 
+
 class FireType(object):
 
     RX = 'rx'
     WF = 'wf'
+
+    VALID_FIRE_TYPES = (RX, WF)
 
 
 class FepsTimeProfiler(BaseTimeProfiler):
@@ -50,7 +53,9 @@ class FepsTimeProfiler(BaseTimeProfiler):
             duff_moisture_content=None):
 
         self._set_times(local_start_time, local_end_time,
-            local_ignition_start_time, local_ignition_end_time, fire_type)
+            local_ignition_start_time, local_ignition_end_time)
+
+        self._set_fire_type(fire_type)
 
         self._relative_humidity = (relative_humidity
             if relative_humidity is not None else self.DEFAULT_RELATIVE_HUMIDITY)
@@ -59,12 +64,10 @@ class FepsTimeProfiler(BaseTimeProfiler):
         self._duff_moisture_content = (duff_moisture_content
             if duff_moisture_content is not None else self.DEFAULT_DUFF_MOISTURE_CONTENT)
 
-        # TODO: implement WF equation
-        if fire_type == FireType.WF:
-            raise NotImplementedError(
-                "FEPS timeprofile not yet implemented for fire type WF")
+        # fire type only comes into play for computing area fractions
+        self._compute_area_fractions()
 
-        getattr(self, '_compute_' + fire_type)()
+        self._compute_hourly_fractions()
 
     @property
     def start(self):
@@ -89,7 +92,7 @@ class FepsTimeProfiler(BaseTimeProfiler):
 
     ## Initialization
 
-    def _set_times(self, start, end, ig_start, ig_end, fire_type):
+    def _set_times(self, start, end, ig_start, ig_end):
         # Makes sure start < end
         self._validate_start_end_times(start, end)
         self._start = start
@@ -130,20 +133,81 @@ class FepsTimeProfiler(BaseTimeProfiler):
             # shrink
             self._ig_end  = min(self._ig_end, self._end)
 
-    ## Rx
+    def _set_fire_type(self, fire_type):
+        fire_type = fire_type.lower()
 
-    def _compute_rx(self):
+        if fire_type not in FireType.VALID_FIRE_TYPES:
+            raise ValueError("Invalid fire type: '{}'.  Valid: '{}')".format(fire_type,
+                "', '".join(FireType.VALID_FIRE_TYPES)))
+
+        self._fire_type = fire_type
+
+    def _compute_area_fractions(self):
+        """Computes the hourly area consumption rate based on equations
+        (19), (20), and (21) in Anderson et. al.  Equation (19) and (20)
+        compute cumulative area at each hour for rx and wf, respectively,
+        and (21) then computes the per-hour area.
+
+            area_i = area_j + (area_k - area_j) * ((hour_i - hour_j) / (hour_k - hour_j))
+            area_i = area_j + (area_k - area_j) * ((hour_i - hour_j)^2 / (hour_k - hour_j)^2)
+            consumption_rate_i = area_i - area_i-1
+
+        Since we're assuming the entire area is consumed between ignition start
+        and ignition end, and since we're assuming total area 1 (since we're
+        only concerned with fractions per hour as opposed to absolute area
+        values), (19) and (20) become
+
+            area_i = (hour_i - hour_j) / (hour_k - hour_j)
+            area_i = (hour_i - hour_j)^2 / (hour_k - hour_j)^2
+
+        Also, since we want, for each hour, the area consumed between hour i
+        and hour i+1, equation (21) becomes
+
+            consumed_i = area_i+1 - area_i
+
+        For Rx, this basically means that the area consumption is divided
+        evenly over the hours of ignition for.  For wf, the growth is
+        exponential.
+
+        We just have to fill in hours outside of the ignition window with zeros.
+
+        Note: partial ignition hours are supported
+        """
+        cumulative_area = []
+        total_ig_seconds = (self._ig_end - self._ig_start).total_seconds()
+        hr = datetime.datetime(self._start.year, self._start.month,
+            self._start.day)
+        final_hr = self._end.date()
+        cumulative_seconds = 0
+        while hr < self._end:
+            hr_end = hr + self.ONE_HOUR
+            overlap_start = max(hr, self._ig_start)
+            overlap_end = min(hr_end, self._ig_end)
+            overlap_seconds = max(0, (overlap_end - overlap_start).total_seconds())
+            cumulative_seconds += overlap_seconds
+            if self._fire_type == FireType.RX:
+                cumulative_area.append(overlap_seconds / total_ig_seconds)
+            else:
+                cumulative_area.append(
+                    math.pow(overlap_seconds, 2) / math.pow(total_ig_seconds, 2))
+            hr += self.ONE_HOUR
+
+        self._area_fractions = []
+        num_hours = len(cumulative_area)
+        for i in range(num_hours):
+            next_val = cumulative_area[i+1] if i < num_hours-1 else 1.0
+            a = next_val = cumulative_area[i]
+            self._area_fractions.append(a)
+
+
+    def _compute_hourly_fractions(self):
         # TODO: make sure start / end times are reasonable for rx?
         sa = self._compute_smoldering_adjustment()
-        area_fractions = self._compute_rx_area_fractions()
         self._hourly_fractions = {
-            "area_fraction": area_fractions,
-            "flaming": self._normalize(
-                self._compute_rx_flaming(area_fractions)),
-            "smoldering": self._normalize(
-                self._compute_rx_short_term_smoldering(area_fractions, sa)),
-            "residual": self._normalize(
-                self._compute_rx_long_term_smoldering(area_fractions, sa))
+            "area_fraction": self._area_fractions,
+            "flaming": self._normalize(self._compute_flaming()),
+            "smoldering": self._normalize(self._compute_short_term_smoldering(sa)),
+            "residual": self._normalize(self._compute_long_term_smoldering(sa))
         }
 
         # TODO: normalize to make sure each set adds up to one
@@ -161,40 +225,6 @@ class FepsTimeProfiler(BaseTimeProfiler):
             * ((100 / self._relative_humidity) / self.RH_b))
 
 
-    def _compute_rx_area_fractions(self):
-        """Computes the hourly area consumption rate based on equations
-        (19) and (21) in Anderson et. al., which are as follows:
-
-            area_i = area_j + (area_k - area_j) * ( (hour_i - hour_j) / (hour_k - hour_j) )
-            consumption_rate_i = area_i - area_i-1
-
-        Since we're assuming the entire area is consumed between ignition start
-        and ignition end, and since we're assuming total area 1 (since we're
-        only concerned with fractions per hour), we get
-
-            consumption_hour_i = 1 / hours_of_ignition
-
-        Which basically means that the area consumption is divided evenly
-        over the hours of ignition.
-
-        We just have to fill in hours outside of the ignition window with zeros.
-
-        Note: partial ignition hours are supported
-        """
-        area_fractions = []
-        total_ig_seconds = (self._ig_end - self._ig_start).total_seconds()
-        hr = datetime.datetime(self._start.year, self._start.month,
-            self._start.day)
-        while hr < self._end:
-            hr_end = hr + self.ONE_HOUR
-            overlap_start = max(hr, self._ig_start)
-            overlap_end = min(hr_end, self._ig_end)
-            overlap_seconds = max(0, (overlap_end - overlap_start).total_seconds())
-            area_fractions.append(overlap_seconds / total_ig_seconds)
-            hr += self.ONE_HOUR
-
-        return area_fractions
-
     K_TFLAM1 = 4 / 3  # TODO: allow user to provide custom val
     K_TFLAM2 = 8  # TODO: allow user to provide custom val
     N_TFLAM = 0.5  # TODO: allow user to provide custom val
@@ -203,7 +233,7 @@ class FepsTimeProfiler(BaseTimeProfiler):
     TFLAM = K_TFLAM1 * K_TFLAM2 * math.pow(D_f, N_TFLAM) / 60
     DECAY_f = 1 / math.pow(math.e, (1/TFLAM))
 
-    def _compute_rx_flaming(self, area_fractions):
+    def _compute_flaming(self):
         """Computes hourly flaming phase consumption, as defined by
         a modification of equation (27) in Anderson et. al.  The original
         equation is as follows:
@@ -233,7 +263,7 @@ class FepsTimeProfiler(BaseTimeProfiler):
         so that DECAY_f, TFLAM, and D_f can be defined as constants, above.
         """
         flaming_fractions = []
-        for i, a in enumerate(area_fractions):
+        for i, a in enumerate(self._area_fractions):
             prev = flaming_fractions[i-1] if i > 0 else 0.0
             f = a * (1 - self.DECAY_f) + prev * self.DECAY_f
             flaming_fractions.append(f)
@@ -248,7 +278,7 @@ class FepsTimeProfiler(BaseTimeProfiler):
     EDR = K_EDR1 * K_EDR2 * math.pow(D_STS, N_EDR) / 60
     DECAY_STS = 1 / math.pow(math.e, (1/EDR))
 
-    def _compute_rx_short_term_smoldering(self, area_fractions, sa):
+    def _compute_short_term_smoldering(self, sa):
         """Computes hourly smoldering (i.e. Short Term Smoldering, STS)
         phase consumption, as defined by a modification of equation (28)
         in Anderson et. al.  The original is as follows
@@ -285,7 +315,7 @@ class FepsTimeProfiler(BaseTimeProfiler):
         """
         sts_fractions = []
 
-        for i, a in enumerate(area_fractions):
+        for i, a in enumerate(self._area_fractions):
             prev = sts_fractions[i-1] if i > 0 else 0.0
             s = sa * a * (1 - self.DECAY_STS) + (prev * self.DECAY_STS)
             sts_fractions.append(s)
@@ -296,7 +326,7 @@ class FepsTimeProfiler(BaseTimeProfiler):
     M_DBM = 130  # TODO: allow user to provide custom val
     K_RDR = 12  # TODO: allow user to provide custom val
 
-    def _compute_rx_long_term_smoldering(self, area_fractions, sa):
+    def _compute_long_term_smoldering(self, sa):
         """Computes hourly residual (i.e. Long Term Smoldering, LTS)
         phase consumption, as defined by a modification of equation (29)
         in Anderson et. al.  The original is as follows
@@ -327,7 +357,7 @@ class FepsTimeProfiler(BaseTimeProfiler):
         rdr = self.K_RDR * inv_lts / ((1 - math.pow(math.e,-1)) / 100)
         decay_l = 1 / math.pow(math.e, 1 / rdr)
 
-        for i, a in enumerate(area_fractions):
+        for i, a in enumerate(self._area_fractions):
             prev = lts_fractions[i-1] if i > 0 else 0.0
             s = sa * a * (inv_lts / 100) * (1 - decay_l) + (prev * decay_l)
             lts_fractions.append(s)
